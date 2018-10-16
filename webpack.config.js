62f04f887bb9fe68b5e097936b6d608131084fa2
Johnny Zarate <jzdev0@gmail.com>
2018-10-15 23:40:05 -0700

    Updated the webpack.config to generate prod files and paths.

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const path = require('path');
 
+const isProduction = env => env !== 'development';
+
 module.exports = (env = 'development') => ({
   mode: env,
   devServer: {
     contentBase: [path.join(__dirname, 'dist')],
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   entry: './src/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    publicPath: isProduction(env) ? 'dist/' : '',
   },
   plugins: [
     new HtmlWebpackPlugin({
       alwaysWriteToDisk: true,
-      filename: path.join(__dirname, './dist/index.html'),
-      template: 'src/template.html'
+      filename: isProduction(env) ?
+        path.join(__dirname, './index.html') :
+        path.join(__dirname, './dist/index.html'),
+      template: 'src/template.html',
     }),
-    new HtmlWebpackHarddiskPlugin()
+    new HtmlWebpackHarddiskPlugin(),
   ]
 });
